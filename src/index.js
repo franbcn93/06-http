88e7ecebeccb70915df77eac6755a1b85ad26312
Francisco <franciscoparisalbero@gmail.com>
2021-02-23 14:42:10 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import * as CRUD from "./js/crud-provider";
+import { init } from "./js/archivos-page";
+// CRUD.getUsuario(1).then(console.log);
+
+// CRUD.crearUsuario({
+//   name: "Paco",
+//   job: "Electrónico",
+// }).then(console.log);
+
+// CRUD.actualizarUsuario(1, {
+//   name: "Yaz",
+//   job: "Integradora Social",
+// }).then(console.log);
+
+// CRUD.borrarUsuario(1).then(console.log);
+
+init();
